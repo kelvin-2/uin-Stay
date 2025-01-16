a5d943a6ef35c4063fb8433ca1e4ff9aab238e68
Kelvin <kelvinyounglee47@gmail.com>
2025-01-16 20:12:18 +0200

    added home page and fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import HomePage from './components/HomePage'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Routes>
+         {/* Default Route: Home Page */}
+       <Route path="/" element={<HomePage/>} />
+      
+      </Routes>
      
     </BrowserRouter>
   );
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBox(){
+    return (
+        <div className="bg-blue-200">
+            hi
+        </div>
+
+    )
+}
+export default  SearchBox; 
