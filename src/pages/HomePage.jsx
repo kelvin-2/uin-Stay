b155f3c5db8953f777da16a01a8ccd7ec72a8979
upenyu mudzingwa <h220087e@hit.ac.zw>
2025-12-20 22:14:37 +0200

    added pi to handle images and create bothe properties

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,17 +17,12 @@ function HomePage() {
 
   return (
     <div className="w-full mx-auto">
-      {/* <HeroSection/> */}
-      
-
+      <HeroSection/>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        
-        
           <>
             <FeaturedProperties/>
             <Benefits/>
           </>
-        
       </div>
     </div>
   );
