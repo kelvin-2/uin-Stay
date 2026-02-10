5a2de7783260d02ebabb2f35fd1e30a8581f5fd4
kelvin <kelvinyounglee47@gmail.com>
2026-02-10 12:20:34 +0200

    fixed something

--- src/api/publicApi.jsx
+++ src/api/publicApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-const API_URL = 'http://localhost:3002/api';
-// const API_URL = 'https://uinstaybackend.onrender.com/api';
+// const API_URL = 'http://localhost:3002/api';
+const API_URL = 'https://uinstaybackend.onrender.com/api';
 const publicApi = axios.create({
   baseURL: API_URL,
   timeout: 40000,
